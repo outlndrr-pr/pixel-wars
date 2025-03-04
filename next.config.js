183880e8dd6be2a80ab7cef865d2f28375fdc2b9
Jesus Perez <cmd@outlndrr.art>
2025-03-04 18:38:58 -0400

    Simplify configuration

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
-        ...config.resolve.fallback,
         fs: false,
         net: false,
         tls: false,
